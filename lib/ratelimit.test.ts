4f52677d3fa1215d8274779aefd04b5699e3186a
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-06-15 11:32:34 +0200

    add rate limiting implementation

---
+++ lib/ratelimit.test.ts
@@ -0,0 +1,188 @@
+import { assertEquals } from "https://deno.land/std@0.143.0/testing/asserts.ts";
+import {
+  Bucket,
+  JoinLimiter,
+  PrivmsgLimiter,
+  SlowModeLimiter,
+  AccountStatus,
+  ChannelRole,
+} from "./ratelimit.ts";
+import { type Channel } from "./client.ts";
+
+Deno.test("Bucket 1 token every 1s", () => {
+  const bucket = new Bucket({ capacity: 1, period: 1000 });
+
+  assertEquals(bucket.get(0), 0);
+  assertEquals(bucket.get(1), 999);
+  assertEquals(bucket.get(1000), 0);
+  assertEquals(bucket.get(1005), 995);
+  assertEquals(bucket.get(2000), 0);
+  assertEquals(bucket.get(2011), 989);
+  assertEquals(bucket.get(3000), 0);
+});
+
+Deno.test("Bucket 20 tokens every 30s", () => {
+  const bucket = new Bucket({ capacity: 20, period: 30 * 1000 });
+
+  for (let i = 0; i < 20; ++i) {
+    assertEquals(bucket.get(0), 0);
+  }
+  assertEquals(bucket.get(0), 30 * 1000);
+
+  for (let i = 0; i < 20; ++i) {
+    assertEquals(bucket.get(30 * 1000), 0);
+  }
+  assertEquals(bucket.get(30 * 1000), 30 * 1000);
+});
+
+Deno.test("Bucket reset when changing period/capacity", () => {
+  const bucket = new Bucket({ capacity: 20, period: 30 * 1000 });
+
+  for (let i = 0; i < 10; ++i) {
+    assertEquals(bucket.get(0), 0);
+  }
+  bucket.setCapacity(30, 0);
+  assertEquals(bucket.get(0), 30 * 1000);
+  for (let i = 0; i < 30; ++i) {
+    assertEquals(bucket.get(30 * 1000), 0);
+  }
+});
+
+Deno.test("Join limiter (normal)", () => {
+  const limiter = new JoinLimiter({ status: AccountStatus.Normal });
+
+  // 20 tokens every 10s
+  for (let i = 0; i < 20; ++i) {
+    assertEquals(limiter.get(0), 0);
+  }
+  assertEquals(limiter.get(0), 10 * 1000);
+
+  for (let i = 0; i < 20; ++i) {
+    assertEquals(limiter.get(10 * 1000), 0);
+  }
+  assertEquals(limiter.get(10 * 1000), 10 * 1000);
+});
+
+Deno.test("Join limiter (verified)", () => {
+  const limiter = new JoinLimiter({ status: AccountStatus.Verified });
+
+  // 2000 tokens every 10s
+  for (let i = 0; i < 2000; ++i) {
+    assertEquals(limiter.get(0), 0);
+  }
+  assertEquals(limiter.get(0), 10 * 1000);
+
+  for (let i = 0; i < 2000; ++i) {
+    assertEquals(limiter.get(10 * 1000), 0);
+  }
+  assertEquals(limiter.get(10 * 1000), 10 * 1000);
+});
+
+Deno.test("Slow mode limiter (viewer) 1s -> 10s", () => {
+  const limiter = new SlowModeLimiter();
+  const channel = "#test";
+
+  // 1 token every 1s
+  assertEquals(limiter.get(0, channel), 0);
+  assertEquals(limiter.get(0, channel), 1000);
+  assertEquals(limiter.get(999, channel), 1);
+
+  // changed to 1 token every 10s
+  // changing period/capacity empties the bucket
+  const options = { slowModeSeconds: 10 };
+  assertEquals(limiter.get(1 * 1000, channel, options), 10 * 1000);
+  assertEquals(limiter.get(10 * 1000, channel, options), 1 * 1000);
+  assertEquals(limiter.get(11 * 1000, channel, options), 0);
+  assertEquals(limiter.get(11 * 1000, channel, options), 10 * 1000);
+  assertEquals(limiter.get(20 * 1000, channel, options), 1 * 1000);
+  assertEquals(limiter.get(21 * 1000, channel, options), 0);
+});
+
+Deno.test("Slow mode limiter (moderator) 1s -> 10s", () => {
+  const limiter = new SlowModeLimiter();
+  const channel = "#test";
+
+  // 1 token every 1s
+  assertEquals(limiter.get(0, channel), 0);
+  assertEquals(limiter.get(0, channel), 1000);
+  assertEquals(limiter.get(999, channel), 1);
+
+  // changed to 1 token every 10s
+  // changing period/capacity empties the bucket
+  // because slow mode has no effect on moderators, it will not be changed
+  // and thus it will not empty the bucket
+  const options = { slowModeSeconds: 10, role: ChannelRole.Moderator };
+  assertEquals(limiter.get(1 * 1000, channel, options), 0);
+  assertEquals(limiter.get(1 * 1000, channel, options), 1 * 1000);
+  assertEquals(limiter.get(2 * 1000, channel, options), 0);
+  assertEquals(limiter.get(2 * 1000, channel, options), 1 * 1000);
+  assertEquals(limiter.get(3 * 1000, channel, options), 0);
+});
+
+Deno.test("Privmsg limiter (normal viewer)", () => {
+  const limiter = new PrivmsgLimiter({ status: AccountStatus.Normal });
+  const channel = "#test";
+  const role = ChannelRole.Viewer;
+
+  // 20 tokens every 30s
+  for (let i = 0; i < 20; ++i) {
+    assertEquals(limiter.get(0, channel, { role }), 0);
+  }
+  assertEquals(limiter.get(0, channel, { role }), 30 * 1000);
+});
+
+Deno.test("Privmsg limiter (normal moderator)", () => {
+  const limiter = new PrivmsgLimiter({ status: AccountStatus.Normal });
+  const channel = "#test";
+  const role = ChannelRole.Moderator;
+
+  // 100 tokens every 30s
+  for (let i = 0; i < 100; ++i) {
+    assertEquals(limiter.get(0, channel, { role }), 0);
+  }
+  assertEquals(limiter.get(0, channel, { role }), 30 * 1000);
+});
+
+Deno.test("Privmsg limiter (verified viewer) global limit", () => {
+  const limiter = new PrivmsgLimiter({ status: AccountStatus.Verified });
+  const channels = Array(7500 / 20)
+    .fill(0)
+    .map((_, i) => `#${i}`) as Channel[];
+  const role = ChannelRole.Viewer;
+
+  // (global) 7500 tokens every 30s
+  let tokens = 7500;
+  for (const channel of channels) {
+    // (per-channel) 20 tokens every 30s
+    for (let i = 0; i < 20; ++i) {
+      assertEquals(limiter.get(0, channel, { role }), 0);
+      tokens -= 1;
+    }
+    assertEquals(limiter.get(0, channel, { role }), 30 * 1000);
+    // @ts-expect-error: accesing private property in test
+    const globalRemaining = limiter._global._tokens;
+    assertEquals(globalRemaining, tokens);
+  }
+});
+
+Deno.test("Privmsg limiter (verified moderator) global limit", () => {
+  const limiter = new PrivmsgLimiter({ status: AccountStatus.Verified });
+  const channels = Array(7500 / 100)
+    .fill(0)
+    .map((_, i) => `#${i}`) as Channel[];
+  const role = ChannelRole.Moderator;
+
+  // (global) 7500 tokens every 30s
+  let tokens = 7500;
+  for (const channel of channels) {
+    // (per-channel) 100 tokens every 30s
+    for (let i = 0; i < 100; ++i) {
+      assertEquals(limiter.get(0, channel, { role }), 0);
+      tokens -= 1;
+    }
+    assertEquals(limiter.get(0, channel, { role }), 30 * 1000);
+    // @ts-expect-error: accesing private property in test
+    const globalRemaining = limiter._global._tokens;
+    assertEquals(globalRemaining, tokens);
+  }
+});
---
+++ lib/ratelimit.ts
@@ -0,0 +1,251 @@
+import { type Channel } from "./client.ts";
+import { sleep } from "./util.ts";
+
+const MILLISECOND = 1;
+const SECOND = MILLISECOND * 1000;
+// const MINUTE = SECOND * 60;
+// const HOUR = MINUTE * 60;
+// const DAY = HOUR * 24;
+// const WEEK = DAY * 7;
+
+export enum AccountStatus {
+  Normal,
+  Known,
+  Verified,
+}
+
+export enum ChannelRole {
+  Viewer,
+  Subscriber,
+  VIP,
+  Moderator,
+  Streamer,
+}
+
+export interface RateLimiter {
+  /**
+   * Returns a promise that resolves when a message can be sent.
+   */
+  privmsg(now: number, channel: Channel): Promise<void>;
+
+  /**
+   * Returns a promise that resolves when a channel can be joined.
+   * @param now
+   */
+  join(now: number): Promise<void>;
+}
+
+/**
+ * Default chat rate limiter with proper handling of known/verified accounts and vip/mod/streamer per-channel roles.
+ * Uses window-based buckets described in the official documentation (https://dev.twitch.tv/docs/irc#rate-limits).
+ */
+export class DefaultLimiter implements RateLimiter {
+  private _privmsg: PrivmsgLimiter;
+  private _slowmode: SlowModeLimiter;
+  private _join: JoinLimiter;
+  private _sleep: (ms: number) => Promise<void> = sleep;
+
+  constructor(options: { status?: AccountStatus } = {}) {
+    this._privmsg = new PrivmsgLimiter(options);
+    this._slowmode = new SlowModeLimiter();
+    this._join = new JoinLimiter(options);
+  }
+
+  privmsg(
+    now: number,
+    channel: Channel,
+    options: { role?: ChannelRole; slowModeSeconds?: number } = {}
+  ): Promise<void> {
+    let delay = 0;
+    delay: {
+      const privmsg = this._privmsg.get(now, channel, options);
+      if (privmsg > 0) {
+        delay = privmsg;
+        break delay;
+      }
+
+      const slowmode = this._slowmode.get(now, channel, options);
+      if (slowmode > 0) {
+        delay = slowmode;
+        break delay;
+      }
+    }
+
+    if (delay > 0) {
+      return this._sleep(delay);
+    } else {
+      return Promise.resolve();
+    }
+  }
+
+  join(now: number): Promise<void> {
+    const remainingMs = this._join.get(now);
+
+    if (remainingMs > 0) {
+      return new Promise((done) => setTimeout(done, remainingMs));
+    } else {
+      return Promise.resolve();
+    }
+  }
+}
+
+/**
+ * Privmsg limiter, handling per-channel message limits based on vip/mod/streamer role,
+ * and global limit for verified bots.
+ *
+ * You should use `DefaultLimiter` unless you have a good reason not to.
+ */
+export class PrivmsgLimiter {
+  private _buckets: Record<Channel, Bucket> = {};
+  private _global?: Bucket;
+
+  constructor(options: { status?: AccountStatus } = {}) {
+    if (options.status === AccountStatus.Verified) {
+      this._global = new Bucket({ capacity: 7500, period: 30 * SECOND });
+    }
+  }
+
+  /**
+   * Returns the remaining number of milliseconds until a token is available.
+   */
+  get(now: number, channel: Channel, options: { role?: ChannelRole } = {}): number {
+    const role = options.role ?? ChannelRole.Viewer;
+    const capacity = role >= ChannelRole.VIP ? 100 : 20;
+
+    this._buckets[channel] ??= new Bucket({ capacity, period: 30 * SECOND });
+    if (this._buckets[channel].capacity !== capacity) {
+      this._buckets[channel].setCapacity(capacity, now);
+    }
+
+    const perChannel = this._buckets[channel].get(now);
+    if (perChannel > 0) {
+      return perChannel;
+    }
+
+    if (this._global) {
+      return this._global?.get(now);
+    }
+
+    return 0;
+  }
+}
+
+/**
+ * Slow mode limiter, handling per-channel slow mode, which defaults to 1s,
+ * with correct treatment of vip/mod/streamer roles.
+ *
+ * You should use `DefaultLimiter` unless you have a good reason not to.
+ */
+export class SlowModeLimiter {
+  private _buckets: Record<Channel, Bucket> = {};
+
+  /**
+   * Returns the remaining number of milliseconds until a token is available.
+   */
+  get(
+    now: number,
+    channel: Channel,
+    options: { slowModeSeconds?: number; role?: ChannelRole } = {}
+  ) {
+    const role = options.role ?? ChannelRole.Viewer;
+    const ms = role >= ChannelRole.VIP ? 1 * SECOND : (options?.slowModeSeconds ?? 1) * SECOND;
+
+    this._buckets[channel] ??= new Bucket({ capacity: 1, period: ms });
+    if (this._buckets[channel].period !== ms) {
+      this._buckets[channel].setPeriod(ms, now);
+    }
+
+    return this._buckets[channel].get(now);
+  }
+}
+
+/**
+ * Join limiter, handling global `JOIN` limits, with correct treatment of known/verified account status.
+ *
+ * You should use `DefaultLimiter` unless you have a good reason not to.
+ */
+export class JoinLimiter {
+  private _bucket: Bucket;
+
+  constructor(options: { status?: AccountStatus } = {}) {
+    this._bucket = new Bucket({
+      capacity: options.status === AccountStatus.Verified ? 2000 : 20,
+      period: 10 * SECOND,
+    });
+  }
+
+  /**
+   * Returns the remaining number of milliseconds until a token is available.
+   */
+  get(now: number) {
+    return this._bucket.get(now);
+  }
+}
+
+/**
+ * Rate limiter which holds a bucket of tokens that periodically refills back to its full capacity.
+ *
+ * This is the data structure used to hand out tokens.
+ */
+export class Bucket {
+  private _lastRefresh: number;
+  private _tokens: number;
+  private _capacity: number;
+  private _period: number;
+
+  constructor(options: { capacity: number; period: number }) {
+    this._capacity = options.capacity;
+    this._period = options.period;
+    this._lastRefresh = 0;
+    this._tokens = this._capacity;
+  }
+
+  /**
+   * Returns the remaining number of milliseconds until a token is available.
+   */
+  get(now: number): number {
+    if (now - this._lastRefresh >= this._period) {
+      this._tokens = this._capacity;
+      this._lastRefresh = now;
+    }
+
+    if (this._tokens > 0) {
+      this._tokens -= 1;
+      return 0;
+    }
+
+    return this._period - (now - this._lastRefresh);
+  }
+
+  /**
+   * Current bucket capacity.
+   */
+  get capacity(): number {
+    return this._capacity;
+  }
+  /**
+   * Changing `capacity` empties the bucket, which means you will have to wait
+   * one period after setting it before being able to receiving any tokens.
+   */
+  setCapacity(value: number, now: number) {
+    this._capacity = value;
+    this._tokens = 0;
+    this._lastRefresh = now;
+  }
+
+  /**
+   * Current bucket refill period.
+   */
+  get period(): number {
+    return this._period;
+  }
+  /**
+   * Changing `period` empties the bucket, which means you will have to wait
+   * one period after setting it before being able to receiving any tokens.
+   */
+  setPeriod(value: number, now: number) {
+    this._period = value;
+    this._tokens = 0;
+    this._lastRefresh = now;
+  }
+}
